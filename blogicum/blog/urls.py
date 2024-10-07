838e4dbd79da0dcdb2870f81cf180f037a55890c
Egor Shkiria <zuhijan@yandex.ru>
2024-10-07 13:50:33 +0100

    add apps pages,blog. add views, urls, templates

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('category/<slug:category_slug>', views.category_posts, name='category_posts'),
+    path('posts/<int:pk>', views.post_detail, name='post_detail'),
+]
